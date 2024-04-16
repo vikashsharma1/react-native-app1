c9f7574b45275a39532c742815348101b78adce0
vikashsharma1 <vikash.sh@outlook.com>
2024-04-16 21:55:54 +0530

    rn-modal-test-file

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Text, View, Button, Modal } from 'react-native';
+
 
 export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "lightgray", padding: 60 }}>
+      <Button title='Click here' onPress={() => setIsOpen(true)} color={'darkgray'} />
+      <Modal visible={isOpen}>
+        <View>
+          <Text>This is modal content!</Text>
+        </View>
+      </Modal>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
