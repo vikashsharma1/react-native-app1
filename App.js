4cb35353769a1e3f2b8ea11f04e53812329c3ed7
vikashsharma1 <vikash.sh@outlook.com>
2024-06-11 06:42:59 +0530

    dimension in react native

--- App.js
+++ App.js
@@ -1,17 +1,37 @@
 import { useState } from 'react';
-import { Text, View, Button, Modal } from 'react-native';
+import { Text, View, StyleSheet, Dimensions } from 'react-native';
 
 
 export default function App() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <View style={{ flex: 1, backgroundColor: "lightgray", padding: 60 }}>
-      <Button title='Click here' onPress={() => setIsOpen(true)} color={'darkgray'} />
-      <Modal visible={isOpen}>
-        <View>
-          <Text>This is modal content!</Text>
-        </View>
-      </Modal>
+    <View style={styles.container}>
+      <View style={styles.box}> 
+        <Text style={styles.text}>Welcome Vikash!</Text>
+      </View>
     </View>
   );
 }
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'plum',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  box: {
+    width: windowWidth > 768 ? '50%' : '70%',
+    height: '40%',
+    backgroundColor: 'lightblue',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    fontSize: windowWidth > 768 ? 42 : 24
+  }
+})
